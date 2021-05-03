4209b9a4605ba99f9a55d83f25d83902fc270b74
Sabrina Ayres <sabrina.marie.ayres@gmail.com>
2021-05-03 11:43:31 -0500

    Adding last portion of the views exercise

--- src/main/java/com/codeup/SpringBlog/controllers/PostController.java
+++ src/main/java/com/codeup/SpringBlog/controllers/PostController.java
@@ -1,24 +1,43 @@
 package com.codeup.SpringBlog.controllers;
 
+import com.codeup.SpringBlog.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
 
     @GetMapping("/posts")
-    @ResponseBody
-    public String posts() {
-        return "posts index page";
+    public String posts(Model model) {
+        List<Post> posts = new ArrayList<>();
+        Post post1 = new Post("My First Blog", "Some content goes here");
+        Post post2 = new Post("My Second Blog", "Some content goes here");
+        posts.add(post1);
+        posts.add(post2);
+        model.addAttribute("posts", posts);
+        return "posts/index";
     }
 
+//    public String posts() {
+//        return "posts index page";
+//    }
+
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String post() {
-        return "view an individual post";
+    public String post(Model model) {
+        Post post = new Post("My First Blog", "Some content goes here");
+        model.addAttribute("post", post);
+        return "posts/show";
     }
+
+//    public String post() {
+//        return "view an individual post";
+//    }
 
     @GetMapping("/posts/create")
     @ResponseBody
