8ddbc5799631c2bb79172761d506097d7cb12df4
Sabrina Ayres <sabrina.marie.ayres@gmail.com>
2021-05-10 12:28:18 -0500

    Adding Security Exercise

--- src/main/java/com/codeup/SpringBlog/controllers/PostController.java
+++ src/main/java/com/codeup/SpringBlog/controllers/PostController.java
@@ -1,8 +1,10 @@
 package com.codeup.SpringBlog.controllers;
 
 import com.codeup.SpringBlog.models.Post;
+import com.codeup.SpringBlog.models.User;
 import com.codeup.SpringBlog.repositories.PostRepository;
 import com.codeup.SpringBlog.repositories.UserRepository;
+import com.codeup.SpringBlog.services.EmailService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -14,10 +16,12 @@ public class PostController {
 
     private final PostRepository postsDao;
     private final UserRepository userDao;
+    private final EmailService emailService;
 
-    public PostController(PostRepository postsDao, UserRepository userDao) {
+    public PostController(PostRepository postsDao, UserRepository userDao, EmailService emailService) {
         this.postsDao = postsDao;
         this.userDao = userDao;
+        this.emailService = emailService;
     }
 
 
@@ -45,11 +49,22 @@ public class PostController {
         return "/posts/create";
     }
 
-    @PostMapping("/posts/create")
-    public String saveNewPost(@ModelAttribute Post post) {
-        postsDao.save(post);
-        return "redirect:/posts";
+
+        @PostMapping("/posts/create")
+    public String create(@ModelAttribute Post post) {
+        User author = userDao.getOne(1L);
+        post.setUser(author);
+        Post savedPost = postsDao.save(post);
+        emailService.prepareAndSend(post, "title", "body");
+        return "redirect:/posts/" + savedPost.getId();
     }
+
+    //    @PostMapping("/posts/create")
+//    public String saveNewPost(@ModelAttribute Post post) {
+//        postsDao.save(post);
+//        return "redirect:/posts";
+//    }
+
 
     @GetMapping("/posts/edit/{id}")
     public String editPostForm(@PathVariable("id") long id, Model model) {
--- src/main/java/com/codeup/SpringBlog/models/User.java
+++ src/main/java/com/codeup/SpringBlog/models/User.java
@@ -29,6 +29,13 @@ public class User {
 
     }
 
+    public User(User copy) {
+        id = copy.id; // This line is SUPER important! Many things won't work if it's absent
+        email = copy.email;
+        username = copy.username;
+        password = copy.password;
+    }
+
     public User(String username, String email, String password) {
         this.username = username;
         this.email = email;
